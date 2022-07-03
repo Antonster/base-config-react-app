bcd04de006ef8e0715e6bd5df8215ba9d212f8eb
Anton Titov <42874257+Antonster@users.noreply.github.com>
2022-07-03 19:11:23 +0300

    chore: removed web-vitals module

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,6 @@ import GlobalStyles from 'src/global-styles';
 import { store } from 'src/store/store';
 
 import App from './app';
-import reportWebVitals from './report-web-vitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -20,8 +19,3 @@ root.render(
     </Provider>
   </StrictMode>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
